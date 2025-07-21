4500180ae8b7fa31d3e9048a20f275b037dc4ffd
Sakshamgalyan <146658307+Sakshamgalyan@users.noreply.github.com>
2025-07-21 17:14:21 +0530

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Paysecure. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Paysecure. Built by Saksham.`,
     },
     prism: {
       theme: prismThemes.github,
